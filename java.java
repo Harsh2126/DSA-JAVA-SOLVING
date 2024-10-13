71379d167022fc64d4e8991ded89659d12d2a19b
Harsh2126 <87963984+Harsh2126@users.noreply.github.com>
2024-10-13 23:36:22 +0530

    FILE CONTAIN SOME STRING & ARRAY QUESTIONS OF JAVA

---
+++ java.java
@@ -0,0 +1,154 @@
+// import java.util.HashMap;
+
+class java{
+    public static void main(String[] args){
+        // #####  remove duplicate element from sorted array...............
+
+        // int arr[]={12,32,35,35,46,47,47,88};
+        // int j=0;
+        // for(int i=0;i<arr.length;i++){
+        //     if (arr[i]!=arr[j]){
+        //         j++;
+        //         int temp=arr[j];
+        //         arr[j ]=arr[i];
+        //         arr[i]=temp;
+        //     }
+        // }
+        // for(int i=0;i<j;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
+
+
+
+
+
+
+
+
+        // #### find frequency of element in array...............
+
+        //  int arr[]={12,13,14,33,13,55,66,44,14,13,12,44};
+        //  HashMap<Integer,Integer> map=new HashMap();
+        //  for(int i=0;i<arr.length;i++){
+        //     if(map.containsKey(arr[i])){
+        //        map.put(arr[i],map.get(arr[i])+1);
+        //     }
+        //     else{
+        //         map.put(arr[i], 1);
+        //     }
+        //  }
+        //  System.out.print(map);
+        
+
+
+
+
+
+
+
+        // ## product of element in array ............
+
+        // int arr[]={1,3,4,5,2,2};
+        // int temp=1;
+        // for(int i=0;i<arr.length;i++){
+        //     temp=temp*arr[i];
+        // }
+        // System.out.print(temp);
+
+
+
+
+
+
+        // ##### remove vovwel from string  ...........................................
+
+        // Scanner sec=new Scanner(System.in);
+        // StringBuilder res = new StringBuilder();
+        // String s=sec.nextLine();
+        // for (int i = 0; i < s.length(); i++) {
+        //     char p = s.charAt(i);
+        //     if (p != 'a' && p != 'e' && p != 'i' && p != 'o' && p != 'u' && 
+        //         p != 'A' && p != 'E' && p != 'I' && p != 'O' && p != 'U') {
+        //         res.append(p);
+        //     }
+        // }
+        // System.out.println(res);
+
+
+
+
+
+        // ###### sum of  two string  ................
+
+        // Scanner sc=new Scanner(System.in);
+        // String s=sc.nextLine();
+        // String s1=sc.nextLine();
+        // int num=0;
+        // int num1=0;
+        // for(int i=0;i<s.length();i++){
+        //     num=num*10+(s.charAt(i)-48);
+        // }
+        // for (int i=0;i<s1.length();i++) {
+        //     num1=num1*10+(s1.charAt(i)-48);
+        // }
+        // int sum=num+num1;
+        // System.out.println(sum);
+
+
+
+
+
+
+
+        // ###### print first char of each word ......................
+
+        // Scanner sc=new Scanner(System.in);
+        // String s=sc.nextLine();
+        // StringBuilder res=new StringBuilder();
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)!=' '){
+        //         // res.append(s.charAt(i+1));
+        //         if (i==0||s.charAt(i-1)==' '){
+        //             System.out.print(s.charAt(i)+" ");
+        //         }
+        //     }
+        // }
+        // // System.out.println(res);
+
+
+
+
+
+
+
+        // #### avg all positive integer  of array.............................
+
+        // Scanner sc=new Scanner(System.in);
+        // int n=sc.nextInt();
+        // int[] arr=new int[n];
+        // for (int i=0;i<n;i++){
+        //     arr[i]=sc.nextInt();
+        // }
+        // int sum=0;
+        // int count=0;
+        // for(int i: arr){
+        //     if(i>0){
+        //         sum+=i;
+        //         count++;
+        //     }
+        // }
+        // double avg=(double)sum/count;
+        // System.out.println(avg);
+
+
+
+
+
+        
+    }
+
+
+
+
+}
+    
